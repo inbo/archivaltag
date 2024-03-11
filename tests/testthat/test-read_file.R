29d91a12e83fdb859ad332cc0f7ba45fc215d117
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-03-11 21:46:07 +0100

    Add tests for file arg

--- tests/testthat/test-read_file.R
+++ tests/testthat/test-read_file.R
@@ -3,37 +3,66 @@ cefas_g5dst_press_temp <- system.file("extdata",
                                       package = "archivaltag"
 )
 
+test_that("file is not a character", {
+  expect_error(read_file(file = 1,
+                         manufacturer = "a",
+                         model = "b",
+                         sensors = "temperature"),
+               regexp = "`file` must be a character.",
+               fixed = TRUE)
+})
+
+test_that("file is a character of length 1", {
+  expect_error(read_file(file = c("a", "b"),
+                         manufacturer = "c",
+                         model = "d",
+                         sensors = "temperature"),
+               regexp = "`file` must be of length 1.",
+               fixed = TRUE)
+})
+
+
 test_that("manufacturer is not a character", {
   expect_error(read_file(file = cefas_g5dst_press_temp,
-                         manufacturer = c(1)),
+                         manufacturer = c(1),
+                         model = "b",
+                         sensors = "temperature"),
                regexp = "`manufacturer` must be a character.",
                fixed = TRUE)
 })
 
 test_that("manufacturer is a character of length 1", {
   expect_error(read_file(file = cefas_g5dst_press_temp,
-                         manufacturer = c("a", "b")),
+                         manufacturer = c("a", "b"),
+                         model = "b",
+                         sensors = "temperature"),
                regexp = "`manufacturer` must be of length 1.",
                fixed = TRUE)
 })
 
 test_that("model is not a character", {
   expect_error(read_file(file = cefas_g5dst_press_temp,
-                         manufacturer = "r", model = 1),
+                         manufacturer = "r",
+                         model = 1,
+                         sensors = "temperature"),
                regexp = "`model` must be a character.",
                fixed = TRUE)
 })
 
 test_that("model is a character of length 1", {
   expect_error(read_file(file = cefas_g5dst_press_temp,
-                         manufacturer = "r", model = c("a", "b")),
+                         manufacturer = "r",
+                         model = c("a", "b"),
+                         sensors = "temperature"),
                regexp = "`model` must be of length 1.",
                fixed = TRUE)
 })
 
 test_that("sensor is not a character", {
   expect_error(read_file(file = cefas_g5dst_press_temp,
-                         manufacturer = "r", model = "r", sensors = 1),
+                         manufacturer = "r",
+                         model = "r",
+                         sensors = 1),
                regexp = "`sensors` must be a character vector.",
                fixed = TRUE)
 })
